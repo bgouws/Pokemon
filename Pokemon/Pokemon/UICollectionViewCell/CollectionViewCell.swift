ab8d92caf7542a8d07c09e7f84f0089bfdceef85
Brandon Gouws <bgouws@jhb.dvt.co.za>
2020-08-02 13:24:50 +0200

    Did some cleaning and added assets

--- Pokemon/Pokemon/UICollectionViewCell/CollectionViewCell.swift
+++ Pokemon/Pokemon/UICollectionViewCell/CollectionViewCell.swift
@@ -14,6 +14,7 @@ class CollectionViewCell: UICollectionViewCell {
     func setUp(pokemon: Pokemon) {
         self.lblName.text = pokemon.name
         guard let urlImage = URL(string: pokemon.sprites.front_default) else { return }
+        imgView.image = nil
         imgView.downloadImage(from: urlImage)
         styleCell()
     }
